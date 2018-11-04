5d9e378e74da33fe91ba0dd1a64feb94bc87269a
Fabio Spampinato <spampinabio@gmail.com>
2018-11-04 18:50:54 +0100

    Ignored dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { result } from './types';
-declare function callSpy(fn: Function, res: result): Function;
-export default callSpy;
--- dist/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare type result = {
-    called: boolean;
-    this?: any;
-    arguments?: any[];
-    return?: any;
-};
-export { result };
